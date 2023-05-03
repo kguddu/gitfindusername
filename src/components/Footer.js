39cd36a4a7d5f29033fdb9335f3cd0d368bf5d8e
guddu <kguddu.1011@gmail.com>
2023-05-03 14:28:03 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@
 const Footer=()=>{
     return(
        <div className="card footer">
-         <h2>GitFind | Developed by Guddu kuamar &amp; Team | &copy; 2022</h2>
+         <h2>GitFind | Developed by Guddu Kumar &amp;  | &copy; 2023</h2>
       <a href="https://github.com/kguddu/gitfindusername" target="_blank" rel="noreferrer">Github Source Code</a>
       <p> Made with ❤️ &amp; 💻</p>
        </div>
